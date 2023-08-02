ff9a6dcd563c3ae833e543abe19d68e285ed3ad5
Nixon <lazaronixon@hotmail.com>
2023-08-02 00:19:28 -0300

    add value too long

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotSaved,   with: :unprocessable_entity
   rescue_from ActiveRecord::NotNullViolation, with: :unprocessable_entity
   rescue_from ActiveRecord::RecordNotUnique,  with: :unprocessable_entity
+  rescue_from ActiveRecord::ValueTooLong,     with: :unprocessable_entity
 
   def unprocessable_entity(error)
     render json: { error: error.message }, status: :unprocessable_entity
