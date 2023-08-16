d4077f485e1329e93147652df819f96bef8ed1b9
Nixon <lazaronixon@hotmail.com>
2023-08-16 01:08:31 -0300

    fix contagem-pessoas

--- config/initializers/action_controller_extensions.rb
+++ config/initializers/action_controller_extensions.rb
@@ -1,6 +1,7 @@
 module ActionController
   class Light < ActionController::Metal
     include AbstractController::Rendering
+    include ActionController::ApiRendering
     include ActionController::Renderers
     include ActionController::Head
     include ActionController::StrongParameters
@@ -8,6 +9,7 @@ module ActionController
 
     use_renderers :json
 
+    ActiveSupport.run_load_hooks(:action_controller_api, self)
     ActiveSupport.run_load_hooks(:action_controller, self)
   end
 end
